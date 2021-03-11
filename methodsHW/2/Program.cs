627cfa586ed9341ea78325441d33be9227f06ea1
Igor <76621236+Cl1nt0n@users.noreply.github.com>
2021-03-11 18:28:25 +0300

    Add files via upload

---
+++ methodsHW/2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        public static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                int index = random.Next(0, i);
+
+                int temp = array[i];
+                array[i] = array[index];
+                array[index] = temp;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(10, 100);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            Shuffle(array);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ methodsHW/methodsHW/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsHW
+{
+    class Program
+    {
+        public static int ReadInt()
+        {
+            bool isInt = false;
+            int number;
+
+            do
+            {
+                isInt = int.TryParse(Console.ReadLine(), out number);
+
+            } while (isInt == false);
+
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReadInt());
+
+            Console.ReadKey();
+        }
+    }
+}
